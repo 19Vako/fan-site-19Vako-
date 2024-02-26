50ad3311b84a00c9b13ff3bb23036ac8969b00a7
19Vako <frymendzordan@gmail.com>
2024-02-26 15:26:07 +0200

    add-sezon-2

--- Breacking-bad.js
+++ Breacking-bad.js
@@ -1,6 +1,41 @@
+class Blocks {
+  constructor(){
+   this.menu  =  document.getElementById('Menu');
+   this.box_1 =  document.getElementById('box-1');
+   this.box_2 =  document.getElementById('box-2');
+   this.box_3 =  document.getElementById('box-3');
+  }
+}
+
+class Element {
+  constructor(){
+    this.p  = document.getElementsByTagName('p');
+    this.h1 = document.getElementsByTagName('h1');
+    this.h2 = document.getElementsByTagName('h2');
+    this.screenWidth      = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    this.series_container = document.getElementById(`linksContainer`);
+    this.series = document.getElementsByClassName(`series`);
+  }
+}
+
+class Buttons {
+  constructor(){
+    this.last_button      = document.getElementsByClassName("last-button");
+    this.next_button      = document.getElementsByClassName("next-button");
+    this.series_button    = document.getElementById(`series-button`);
+    this.series_container = document.getElementById(`linksContainer`);
+    this.video_buttonvar  = document.getElementById(`video-button`);
+  }
+}
+
+
+
+
+
+/*
 var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var menu = document.getElementById('Menu');
-var box_1 = document.getElementById('box-1');
+var box_1 =  document.getElementById('box-1');
 var box_2 = document.getElementById('box-2')
 var box_3 = document.getElementById('box-3');
 var ff = document.getElementsByClassName('ff');
@@ -9,11 +44,11 @@ var h1 = document.getElementsByTagName('h1');
 var h2 = document.getElementsByTagName('h2');
 var last_button = document.getElementsByClassName("last-button");
 var next_button = document.getElementsByClassName("next-button");
-var series_button = document.getElementById(`series-button`);
+var series_button = document.getElementById(`series-button`); 
 var series_container = document.getElementById(`linksContainer`);
 var series = document.getElementsByClassName(`series`);
 var video_buttonvar = document.getElementById(`video-button`);
-
+*/
 
 function toggleLinks() {
   var linksContainer = document.getElementById("linksContainer");
@@ -54,37 +89,37 @@ function ScreenAdaptation (width){
   if(width <= 1000){
     
     for(var i = 0; i < ff.length; i++){
-      ff[i].style.cssText = "width: 100%; border-bottom: none; height: 100px; background: #326B3D; font-size: 60px; border-radius: 0; border: 1px solid black; ";
+      Element.ff[i].style.cssText = "width: 100%; border-bottom: none; height: 100px; background: #326B3D; font-size: 60px; border-radius: 0; border: 1px solid black; ";
     }
     for(var i = 0; i < series.length; i++){
-      series[i].style.cssText = `font-size: 50px; width: 100%`
+      Element.series[i].style.cssText = `font-size: 50px; width: 100%`
     }
 
     for(var i = 0; i < last_button.length; i++){
-      last_button[i].style.cssText = "width: 260px, height: 110px; display: flex; alingn-items: center; justify-content: center";
+      Element.last_button[i].style.cssText = "width: 260px, height: 110px; display: flex; alingn-items: center; justify-content: center";
     }
     for(var i = 0; i < next_button.length; i++){
-      next_button[i].style.cssText = "width: 260px, height: 110px; display: flex; alingn-items: center; justify-content: center";
+      Element.next_button[i].style.cssText = "width: 260px, height: 110px; display: flex; alingn-items: center; justify-content: center";
     }
       
-      menu.style.display = "contents";
-      box_1.style.display = "none";
-      box_2.style.cssText = "margin-left: 0; margin-right: 0";
-      box_3.style.cssText = "position: fixed; margin-left: 70%; margin-top: -110%; min-width: 254px; max-width: 254px; height: 302px";
+      Blocks.menu.style.display  = "contents";
+      Blocks.box_1.style.display = "none";
+      Blocks.box_2.style.cssText = "margin-left: 0; margin-right: 0";
+      Blocks.box_3.style.cssText = "position: fixed; margin-left: 70%; margin-top: -110%; min-width: 254px; max-width: 254px; height: 302px";
       
 
-    for(var i = 0; i < p.length; i++ ){
-      p[i].style.fontSize = "50px";
+    for(var i = 0; i < Element.p.length; i++ ){
+      Element.p[i].style.fontSize = "50px";
     }
-    for(var i = 0; i < h1.length; i++ ){
-      h1[i].style.fontSize = "74px";
+    for(var i = 0; i < Element.h1.length; i++ ){
+      Element.h1[i].style.fontSize = "74px";
     }
-    for(var i = 0; i < h2.length; i++ ){
-      h2[i].style.fontSize = "65px";
+    for(var i = 0; i < Element.h2.length; i++ ){
+      Element.h2[i].style.fontSize = "65px";
     } 
-      series_container.style.cssText = `top: 108px; width: 210px`
-      series_button.style.cssText = `font-size: 63px`
-      video_buttonvar.style.cssText = `width: 140px`
+      Buttons.series_container.style.cssText = `top: 108px; width: 210px`
+      Buttons.series_button.style.cssText  = `font-size: 63px`
+      Buttons.video_buttonvar.style.cssText = `width: 140px`
   }
 }
 ScreenAdaptation(screenWidth);
